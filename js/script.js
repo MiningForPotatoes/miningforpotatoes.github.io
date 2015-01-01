220c638057fa83c979e425ea7b1bf026962b7cca
Jay Len <didierlennon@gmail.com>
2015-01-01 23:47:02 +0100

    New website! :tada:

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var isOpen = false;
+function toggleNav(){
+  if (!isOpen){
+    $('#outer-nav-control').attr('data-icon','close');
+    isOpen = true;
+    $('#bar-1,#bar-3').addClass('hide-hamburger-line');
+    $('#bar-2').addClass('bar-2-x');
+    $('#bar-4').addClass('bar-4-x');
+
+	$("#links").removeClass("animated rotateOutUpLeft");
+	$("#links").addClass("animated rotateInUpLeft");
+	$("#links").removeClass("hidden");
+  } else {
+    $('#outer-nav-control').attr('data-icon','hamburger');
+    isOpen = false;
+    $('#bar-1,#bar-3').removeClass('hide-hamburger-line');
+    $('#bar-2').removeClass('bar-2-x');
+    $('#bar-4').removeClass('bar-4-x');
+
+	$("#links").removeClass("animated rotateInUpLeft");
+	$("#links").addClass("animated rotateOutUpLeft");
+
+	$("#links").addClass("hidden");
+  }
+}
+$('.navigation').click(function(){
+  toggleNav();
+});
