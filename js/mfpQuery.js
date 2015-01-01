ac57d19f17b42491c21868451f250783d2f51ad9
Jay Len <didierlennon@gmail.com>
2015-01-01 04:12:19 +0100

    Fixed a bug

--- js/mfpQuery.js
+++ js/mfpQuery.js
@@ -4,22 +4,22 @@ var query = document.getElementById("query");
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "http://198.50.146.38/ping.php?ip=" + ip, true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    data = JSON.parse(xhr.responseText);
-    if (data.max) {
-      query.innerHTML = data.online + "/" + data.max + "<br/>";
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+        data = JSON.parse(xhr.responseText);
+        if (data.max) {
+            query.innerHTML = data.online + "/" + data.max + "<br/>";
 
-      for (i = 0; i <= data.online; i++){
-        query.innerHTML += "<img src=\"http://cravatar.eu/head/" + data.sample[i].name + "/48\" data-tooltip=\"text\" title=\"" + data.sample[i].name + "\"></img>";
-      };
-    } else {
-      query.innerHTML = "The server is Offline :(";
+            for (i = 0; i <= data.online; i++) {
+                query.innerHTML += "<img src=\"http://cravatar.eu/head/" + data.sample[i].name + "/48\" data-tooltip=\"text\" title=\"" + data.sample[i].name + "\"></img>";
+            };
+        } else {
+            query.innerHTML = "The server is Offline :(";
+        }
     }
-  }
 }
 xhr.send();
 
 setInterval(function () {
-  xhr.send
+    xhr.send
 }, 300000);
