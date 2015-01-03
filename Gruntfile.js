42982e0d86aaf14078c3829a9b36e17ed205ca24
jaylen14 <didierlennon@gmail.com>
2015-01-03 22:36:53 +0100

    Speed optimization.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,19 +12,34 @@ module.exports = function(grunt) {
 		},
 
 		watch: {
+            options: {
+                dateFormat: function(time) {
+                    var tm = new Date();
+                    var hms = tm.getHours() + ":" + tm.getMinutes() + ":" + tm.getSeconds();
+                    var dt = tm.getDate() + "/" + tm.getMonth() + 1 + "/" + tm.getFullYear();
+                    grunt.log.writeln('Done in ' + time + 'ms at ' + hms.bold + ' on ' + dt.bold);
+                    grunt.log.writeln('Waiting for more changes...'.cyan);
+                },
+                spawn: false,
+              },
 			css: {
-				files: '**/*.scss',
-				tasks: ['compass']
-			}
+                files: ['**/*.scss'],
+                tasks: ['compass'],
+            },
+
+            js: {
+                files: ['js/src/*.js','Gruntfile.js'],
+                tasks: ['uglify'],
+            },
 		},
 
-//		coffee: {
-//  			compile: {
-//    			files: {
-//      				'js/script.js': 'coffee/script.coffee', // 1:1 compile into single file
-//    			}
-//  			}
-//		}
+		coffee: {
+  			compile: {
+    			files: {
+      				'js/script.js': 'coffee/script.coffee', // 1:1 compile into single file
+    			}
+  			}
+		},
 
         uglify: {
             options: {
@@ -36,22 +51,24 @@ module.exports = function(grunt) {
                 src: ['js/libs/*.js','js/src/*.js'],
                 dest: 'js/global.js'
             }
-        }
+        },
 
-//        connect: {
-//            server: {
-//                options: {
-//                    port: 8000,
-//                    hostname: 'mfp.dev',
-//                    base: '',
-//                }
-//            }
-//        }
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    hostname: 'miningforpotatoes',
+                    base: '../miningforpotatoes.github.io',
+                }
+            }
+        }
 	});
+
+
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-    //grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-	grunt.registerTask('default',['watch','uglify']);
+	grunt.registerTask('default',['uglify','connect','watch']);
 }
